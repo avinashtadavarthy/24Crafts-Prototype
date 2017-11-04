48979e3cbe7f080e57d95ea4bb014029bdb4a086
Avinash Tadavarthy <avinash.tadavarthy@gmail.com>
2017-11-04 20:46:30 +0530

    craftsmen integration 1 - avi

--- app/src/main/java/com/example/iyashwant/spiderprojectprototype/StartingScreen.java
+++ app/src/main/java/com/example/iyashwant/spiderprojectprototype/StartingScreen.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.iyashwant.spiderprojectprototype.Registration.signup;
+
 public class StartingScreen extends AppCompatActivity {
 
     @Override
