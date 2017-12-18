2bd3d49f5c45e4fe5ea11ffd4c988639146b2c3a
Avinash Tadavarthy <avinashtadavarthy@gmail.com>
2017-12-18 16:15:30 +0530

    avi - 18dec - 2

--- app/src/main/java/com/example/iyashwant/spiderprojectprototype/ProfileView.java
+++ app/src/main/java/com/example/iyashwant/spiderprojectprototype/ProfileView.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.animation.BounceInterpolator;
@@ -14,7 +15,6 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-
 public class ProfileView extends AppCompatActivity {
 
     ImageButton edit_profile, profile_back, fav_profile;
@@ -25,6 +25,10 @@ public class ProfileView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         edit_profile = (ImageButton) findViewById(R.id.edit_profile);
         profile_back = (ImageButton) findViewById(R.id.profile_back);
