7a12ebdb5295f824765764c6ddebd55add430c28
rakave <velu.ravichandran@chicagobooth.edu>
2018-02-26 12:26:37 +0530

    For Hari to Pull

--- app/src/main/java/com/twenty/four/crafts/Settings.java
+++ app/src/main/java/com/twenty/four/crafts/Settings.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
