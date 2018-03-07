f43030edffd9178ff0231340c8af44a74afddfff
rakave <velu.ravichandran@chicagobooth.edu>
2018-03-07 19:42:03 +0530

    SetImageBitmap for Client Verification - Done. WIP : Volley URL Retrieval

--- app/src/main/java/com/twenty/four/crafts/ClientVerification1Activity.java
+++ app/src/main/java/com/twenty/four/crafts/ClientVerification1Activity.java
@@ -1,15 +1,25 @@
 package com.twenty.four.crafts;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ClientVerification1Activity extends AppCompatActivity {
 
@@ -40,6 +50,48 @@ public class ClientVerification1Activity extends AppCompatActivity {
             }
         });
 
+        //Vollley for Image Retrieval - TO BE DONE
+        String url = "http://24crafts.cf:3000/users/5a9e814e0a462e6e1f7fa9c5/photos/24 Logo.png";
+        ImageView imageView = findViewById(R.id.profileImg);
+        new ImageLoadTask(url, imageView).execute();
+
+
+    }
+
+    public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
+
+        private String url;
+        private ImageView imageView;
+
+        public ImageLoadTask(String url, ImageView imageView) {
+            this.url = url;
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            try {
+                URL urlConnection = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) urlConnection
+                        .openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            imageView.setImageBitmap(result);
+        }
 
     }
 }
+
+
