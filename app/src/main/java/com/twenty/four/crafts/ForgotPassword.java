5a7cd6dc88d91c1b87341c604b8322a7dca9a186
RakeshVaideeswaran <raku18998@gmail.com>
2018-03-17 23:20:08 +0530

    forgot password with volley completed

--- app/src/main/java/com/twenty/four/crafts/ForgotPassword.java
+++ app/src/main/java/com/twenty/four/crafts/ForgotPassword.java
@@ -1,15 +1,16 @@
 package com.twenty.four.crafts;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.View;
@@ -17,8 +18,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.twenty.four.crafts.registration.StartingScreen;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -26,7 +36,10 @@ public class ForgotPassword extends AppCompatActivity {
     RelativeLayout parent;
     int emailFound = 1;
     EditText regEmailId;
-    String dialogtext,dialogbuttontext;
+    String emailID = "";
+    String dialogtext = "Please check your email for further instructions";
+    String jwtToken = "";
+    String text = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,41 +66,115 @@ public class ForgotPassword extends AppCompatActivity {
             public void onClick(View view) {
               //  Intent i = new Intent(getApplicationContext(),ForgotPasswordPopUp.class);
                 //startActivity(i);
-                if(emailFound == 0)
-                {
-                    dialogtext = getString(R.string.unreg_email_text);
-                    dialogbuttontext = "REGISTER";
-                }
+
+                emailID = regEmailId.getText().toString();
+
+                if(emailID.equals(""))
+                    Toast.makeText(ForgotPassword.this, "Enter a valid email ID", Toast.LENGTH_SHORT).show();
 
                 else
-                {
-                    dialogtext = getString(R.string.reg_email_text);
-                    dialogbuttontext = "OK";
-                }
+                    routeToForgotPassword(emailID);
+            }
+        });
+    }
 
-                final AlertDialog dialog = new AlertDialog.Builder(new ContextThemeWrapper(ForgotPassword.this,R.style.AlertDialog)).setMessage(dialogtext).
-                setPositiveButton(dialogbuttontext, new DialogInterface.OnClickListener() {
+
+    public void alertDialog()
+    {
+        final AlertDialog dialog = new AlertDialog.Builder(new ContextThemeWrapper(ForgotPassword.this,R.style.AlertDialog)).setMessage(dialogtext).
+                setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        if(emailFound == 0)
-                        {
-                            Intent intent = new Intent(ForgotPassword.this, StartingScreen.class);
-                            startActivity(intent);
-                        }
-
-                        else
 
                             dialogInterface.cancel();
                     }
                 })
                 .show();
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-                TextView textView = (TextView) dialog.findViewById(android.R.id.message);
-                textView.setGravity(Gravity.LEFT);
-                textView.setTextSize(15);
+        TextView textView = (TextView) dialog.findViewById(android.R.id.message);
+        textView.setGravity(Gravity.LEFT);
+        textView.setTextSize(15);
+
+    }
+
+    private void routeToForgotPassword(final String emailID) {
+
+        text = emailID;
+
+        jwtToken = getSPData("jwtToken");
+
+        String newurl = User.getInstance().BASE_URL + "user/update/reset_password";
+
+        StringRequest getRequest = new StringRequest(Request.Method.POST, newurl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.e("forgotPassword",response);
+
+               // Toast.makeText(ForgotPassword.this, response, Toast.LENGTH_SHORT).show();
+
+                checkForResponse(response);
+
             }
-        });
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }){
+            @Override
+            public Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<String, String>();
+
+                params.put("email", text);
+
+                return params;
+            }
+        };
+
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(getRequest);
+
+
+
+
+    }
+
+    private void checkForResponse(String response) {
+
+        if(response.equals("successfully updated features."))
+            alertDialog();
+
+
+        else
+        {
+            Toast.makeText(ForgotPassword.this, "Email not found. Kindly Register", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(ForgotPassword.this, StartingScreen.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
+    //Shared Preferences
+    private void storeSPData(String key, String data) {
+
+        SharedPreferences mUserData = this.getSharedPreferences("UserData", MODE_PRIVATE);
+        SharedPreferences.Editor mUserEditor = mUserData.edit();
+        mUserEditor.putString(key, data);
+        mUserEditor.commit();
+
+    }
+
+    private String getSPData(String key) {
+
+        SharedPreferences mUserData = this.getSharedPreferences("UserData", MODE_PRIVATE);
+        String data = mUserData.getString(key, "");
+
+        return data;
+
     }
 }
 
