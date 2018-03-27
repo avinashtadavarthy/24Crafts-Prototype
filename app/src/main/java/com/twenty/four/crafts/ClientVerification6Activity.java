3bb1badfb811f2f2cb0797fc98d6d4dc7386078e
hariharan <hariharan@geass.technology>
2018-03-27 14:00:49 +0600

    Client Verification Layouts

---
+++ app/src/main/java/com/twenty/four/crafts/ClientVerification6Activity.java
@@ -0,0 +1,14 @@
+package com.twenty.four.crafts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientVerification6Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_verification6);
+
+    }
+}
